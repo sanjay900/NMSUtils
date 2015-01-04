9a3e16dee7b26c846ac4d86fb9bb67b83f2a883b
sanjay900 <sanjay.govind9@gmail.com>
2015-01-05 11:28:38 +1300

    Refactor everything pt2

--- nmsUtil-API/src/main/java/com/sk89q/utils/jinglenoteshaded/Instrument.java
+++
@@ -1,6 +0,0 @@
-package com.sk89q.utils.jinglenoteshaded;
-
-public enum Instrument {
-
-    GUITAR, PIANO, BASS, BASS_GUITAR, STICKS, BASS_DRUM, SNARE_DRUM;
-}
--- nmsUtil-API/src/main/java/org/PortalStick/util/Button.java
+++
@@ -1,5 +0,0 @@
-package org.PortalStick.util;
-
-public enum Button {
-	UP,DOWN,LEFT,RIGHT
-}
